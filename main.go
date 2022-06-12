29eed0dcefdeed8447ba44fb1b9dfa929969fb73
cglotr <amir.ariffin.1992@gmail.com>
2022-06-12 16:53:38 +0800

    init

---
+++ main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"database/sql"
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/arikama/go-arctic-tern/arctictern"
+	"github.com/arikama/go-mysql-test-container/mysqltestcontainer"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/hooligram/kifu"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		kifu.Fatal(err.Error())
+	}
+	db, err := getDb()
+	if err != nil {
+		kifu.Fatal(err.Error())
+	}
+	err = db.Ping()
+	if err != nil {
+		kifu.Fatal(err.Error())
+	}
+}
+
+func getDb() (*sql.DB, error) {
+	mysqlUsername := os.Getenv("MYSQL_USERNAME")
+	mysqlPassword := os.Getenv("MYSQL_PASSWORD")
+	mysqlIp := os.Getenv("MYSQL_IP")
+	mysqlPort := os.Getenv("MYSQL_PORT")
+	mysqlDatabase := os.Getenv("MYSQL_DATABASE")
+
+	if isTestEnv() {
+		result, _ := mysqltestcontainer.Start("test")
+		mysqlUsername = result.Username
+		mysqlPassword = result.Password
+		mysqlIp = result.Ip
+		mysqlPort = result.Port
+		mysqlDatabase = result.Database
+	}
+
+	dataSourceName := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v", mysqlUsername, mysqlPassword, mysqlIp, mysqlPort, mysqlDatabase)
+	dataSourceName += "?charset=utf8mb4"
+	dataSourceName += "&collation=utf8mb4_unicode_ci"
+
+	db, err := sql.Open("mysql", dataSourceName)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	migrationDir := "./migration"
+	err = arctictern.Migrate(db, migrationDir)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func isTestEnv() bool {
+	return flag.Lookup("test.v") != nil
+}
---
+++ migration/V002.sql
@@ -0,0 +1,9 @@
+CREATE TABLE translation (
+  id           INT           NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  sura_id      INT           NOT NULL,
+  verse_id     INT           NOT NULL,
+  name         VARCHAR(16)   NOT NULL,
+  translation  VARCHAR(2048) NOT NULL,
+  CONSTRAINT UNIQUE KEY (sura_id, verse_id, name),
+  CONSTRAINT FOREIGN KEY (sura_id, verse_id) REFERENCES quran (sura_id, verse_id)
+);
