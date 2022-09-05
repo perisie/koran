23556d8a2e9b7072c7a51193a109a3451d6db9d4
cglotr <amir.ariffin.1992@gmail.com>
2022-09-05 23:20:13 +0800

    test invalid dir

--- managers/quran_manager_impl_test.go
+++ managers/quran_manager_impl_test.go
@@ -17,8 +17,18 @@ func TestGetVerse(t *testing.T) {
 	s1v1, err := quranManager.GetVerse(1, 1)
 	assert.Nil(t, err)
 	assert.Equal(t, "بِسْمِ اللَّهِ الرَّحْمَٰنِ الرَّحِيمِ", s1v1.Text)
+	assert.Equal(t, 1, len(s1v1.Translations))
+	assert.Equal(t, "In the name of Allah, the Beneficent, the Merciful.", s1v1.Translations["pickthall"])
 
 	s114v6, err := quranManager.GetVerse(114, 6)
 	assert.Nil(t, err)
 	assert.Equal(t, "مِنَ الْجِنَّةِ وَالنَّاسِ", s114v6.Text)
+	assert.Equal(t, 1, len(s114v6.Translations))
+	assert.Equal(t, "Of the jinn and of mankind.", s114v6.Translations["pickthall"])
+}
+
+func TestInvalidCsvDir(t *testing.T) {
+	_, err := managers.NewQuranManagerImpl("./../invalid")
+	assert.NotNil(t, err)
+	assert.Equal(t, "open ./../invalid/quran.csv: no such file or directory", err.Error())
 }
