31c3e00d415b0ba17f680536c9231713d7f3dd1b
cglotr <amir.ariffin.1992@gmail.com>
2022-09-07 22:39:19 +0800

    add tests to manager

--- managers/quran_manager_impl_test.go
+++ managers/quran_manager_impl_test.go
@@ -26,3 +26,41 @@ func TestGetVerse(t *testing.T) {
 	assert.Equal(t, 1, len(s114v6.Translations))
 	assert.Equal(t, "Of the jinn and of mankind.", s114v6.Translations["pickthall"])
 }
+
+func TestGetSurah(t *testing.T) {
+	var quranManager managers.QuranManager
+	var err error
+
+	quranManager, err = managers.NewQuranManagerImpl("./../qurancsv")
+	assert.Nil(t, err)
+
+	surah, err := quranManager.GetSurah(114)
+	assert.Nil(t, err)
+	assert.Equal(t, 6, len(surah.Verses))
+	assert.Equal(t, "مِنَ الْجِنَّةِ وَالنَّاسِ", surah.Verses[5].Text)
+}
+
+func TestGetSurahNotExist(t *testing.T) {
+	var quranManager managers.QuranManager
+	var err error
+
+	quranManager, err = managers.NewQuranManagerImpl("./../qurancsv")
+	assert.Nil(t, err)
+
+	_, err = quranManager.GetSurah(115)
+	assert.NotNil(t, err)
+}
+
+func TestGetSurahInfos(t *testing.T) {
+	var quranManager managers.QuranManager
+	var err error
+
+	quranManager, err = managers.NewQuranManagerImpl("./../qurancsv")
+	assert.Nil(t, err)
+
+	surahInfos, err := quranManager.GetSurahInfos()
+	assert.Nil(t, err)
+	assert.Equal(t, 114, len(surahInfos))
+	assert.Equal(t, "The Opening", surahInfos[0].English)
+	assert.Equal(t, "Mankind", surahInfos[113].English)
+}
