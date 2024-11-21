83a718bdebbb48e451141505d80654999aa8341b
cglotr <aaryfn@gmail.com>
2024-11-21 15:56:15 -0800

    fix deploy deleting data

--- deploy.sh
+++ deploy.sh
@@ -15,12 +15,14 @@ for HOST in melchior; do
     HOST_IP=${host_to_ip[$HOST]}
     echo "> („• ֊ •„) deploying $HOST"
     echo "> deleting old files..."
-    ssh -o StrictHostKeyChecking=no -i $HOST.pem ec2-user@$HOST_IP "mkdir -p ~/app; mkdir -p ~/app/koran"
-    ssh -o StrictHostKeyChecking=no -i $HOST.pem ec2-user@$HOST_IP "cd ~/app/koran; rm -rf *;"
+    ssh -o StrictHostKeyChecking=no -i $HOST.pem ec2-user@$HOST_IP "mkdir -p ~/build; mkdir -p ~/build/koran"
+    ssh -o StrictHostKeyChecking=no -i $HOST.pem ec2-user@$HOST_IP "cd ~/build/koran; rm -rf *;"
     echo "> uploading new files..."
-    scp -o StrictHostKeyChecking=no -i $HOST.pem -r ./* ec2-user@$HOST_IP:~/app/koran
+    scp -o StrictHostKeyChecking=no -i $HOST.pem -r ./* ec2-user@$HOST_IP:~/build/koran
     echo "> starting the service..."
-    ssh -o StrictHostKeyChecking=no -i $HOST.pem ec2-user@$HOST_IP "cd ~/app/koran; go build; pkill koran"
+    ssh -o StrictHostKeyChecking=no -i $HOST.pem ec2-user@$HOST_IP "cd ~/build/koran; go build; pkill koran"
+    ssh -o StrictHostKeyChecking=no -i $HOST.pem ec2-user@$HOST_IP "mkdir -p ~/app; mkdir -p ~/app/koran"
+    ssh -o StrictHostKeyChecking=no -i $HOST.pem ec2-user@$HOST_IP "mv ~/build/koran/koran ~/app/koran"
     ssh -o StrictHostKeyChecking=no -i $HOST.pem ec2-user@$HOST_IP "cd ~/app/koran; ./koran > ./log.txt 2>&1 &"
     echo "> (^ヮ^)/ $HOST up!"
 done
